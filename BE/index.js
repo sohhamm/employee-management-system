596e9f8c1c6ad39de8f49ba36d5ac841ae88685e
Soham Sarkar <soham.ss1@gmail.com>
2021-02-11 11:56:33 +0530

    server dependencies

--- BE/index.js
+++ BE/index.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
+const CONNECTION_URL = process.env.URI;
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello Humaps');
 });
 
-app.listen(PORT, () => {
-  console.log(`listening successfully at PORT:${PORT}`);
-});
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`listening on Port: ${PORT}`)))
+  .catch((error) => console.log(error));
+
+mongoose.set('useFindAndModify', false);
